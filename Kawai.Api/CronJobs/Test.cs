e1242c20d36536c891b0f2df8db2dfdd078fd558
mrizkyridwansyah <mrizkyridwansyah29@gmail.com>
2025-08-12 16:56:22 +0700

    contoh cronjob

--- Kawai.Api/CronJobs/Test.cs
+++ Kawai.Api/CronJobs/Test.cs
@@ -1,4 +1,6 @@
 ﻿using Kawai.Api.Shared;
+using Kawai.Data.SqlConnections;
+using Kawai.Domain.Interfaces;
 
 namespace Kawai.Api.CronJobs;
 
@@ -9,9 +11,21 @@ namespace Kawai.Api.CronJobs;
 [CronJob("* * * * *")]
 public class Test: BaseCronJob
 {
+    private readonly IItemRepository _itemRepository;
+
+    public Test(IItemRepository itemRepository)
+    {
+        _itemRepository = itemRepository;
+    }
+
     public override Task Run()
     {
+        var result = _itemRepository.GetAll(new Domain.Shared.RequestParameter());  
         Console.WriteLine("Test cron job executed at: " + DateTime.Now);
+        Console.WriteLine(string.Join("\n", result.Result.Select(p => $"ItemCode: {p.ItemCode}, ItemName: {p.ItemName}")));
+
+
+
         // Implement your cron job logic here
         return default;
     }
