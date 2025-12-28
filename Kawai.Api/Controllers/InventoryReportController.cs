269713925f01d8ef8c9247d0f5d376161512018e
ilham <ilham@tos.co.id>
2025-12-28 22:56:24 +0700

    <Update><Inventory Report>
    Add New Form

---
+++ Kawai.Api/Controllers/InventoryReportController.cs
@@ -0,0 +1,93 @@
+﻿using ClosedXML.Excel;
+using Kawai.Api.Services;
+using Kawai.Domain.DTOs.Log;
+using Kawai.Domain.Interfaces;
+using Kawai.Domain.Models;
+using Kawai.Domain.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kawai.Api.Controllers;
+
+[Authorize]
+[Route("api/inventory-report")]
+[ApiController]
+public class InventoryReportController : HahaController
+{
+    private readonly IInventoryReportRepository _InventoryReportRepository;
+    private readonly DataLogger _logger;
+
+    public InventoryReportController(IInventoryReportRepository InventoryReportRepository, DataLogger logger)
+    {
+        _InventoryReportRepository = InventoryReportRepository;
+        _logger = logger;
+    }
+
+    [HttpPost("list")]
+    public async Task<IActionResult> List([FromBody] RequestParameter parameter)
+    {
+        var results = await _InventoryReportRepository.GetAll(parameter);
+        return DataTableResult(parameter, results);
+    }
+
+    [HttpPost("export/excel")]
+    public async Task<IActionResult> ExportExcel([FromBody] RequestParameter parameter)
+    {
+        var results = await _InventoryReportRepository.GetAll(parameter);
+        if (results == null || !results.Any()) return NoContent();
+
+        using var workbook = new XLWorkbook();
+        var ws = workbook.Worksheets.Add("Data");
+
+        int rowIdx = 1;
+
+        List<string> headers = ["Warehouse", "Product Code", "Product Name", "Lot No",
+                "Pre Month", "Receipt", "Supply", "Loss / Reject","Current","Inventory","Remarks","User"];
+        ExcelHelper.SetHeader(ws, rowIdx, headers);
+
+        foreach (var result in results)
+        {
+            rowIdx++;
+            var row = ws.Row(rowIdx);
+            int colIdx = 1;
+
+            ExcelHelper.SetCell(row, colIdx, result.Warehouse);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.ProductCode);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.ProductName);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.LotNo);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.PreMonth);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.Receipt);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.Supply);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.LossReject);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.Current);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.Inventory);
+            colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.Remarks);
+            colIdx++;
+            //ExcelHelper.SetCell(row, colIdx, result.LastUpdate.HasValue ? result.LastUpdate.Value.ToString("dd MMM yyyy HH:mm") : "");
+            //colIdx++;
+            ExcelHelper.SetCell(row, colIdx, result.LastUser);
+        }
+
+        ExcelHelper.AutofitColumns(ws, 1, headers.Count);
+
+        var range = ws.Range(1, 1, rowIdx, headers.Count);
+        ExcelHelper.SetBorders(range);
+
+        using var ms = new MemoryStream();
+        workbook.SaveAs(ms);
+        var fileBytes = ms.ToArray();
+        var base64File = Convert.ToBase64String(fileBytes);
+
+        return Success(base64File);
+    }
+}
---
+++ Kawai.Data/Repositories/InventoryReportRepository.cs
@@ -0,0 +1,24 @@
+﻿using Kawai.Data.SqlConnections;
+using Kawai.Domain.DTOs;
+using Kawai.Domain.Interfaces;
+using Kawai.Domain.Models;
+using Kawai.Domain.Shared;
+
+namespace Kawai.Data.Repositories;
+
+public class InventoryReportRepository : IInventoryReportRepository
+{
+    private readonly DbExecutor _dbExecutor;
+
+    public InventoryReportRepository(DbExecutor dbExecutor)
+    {
+        _dbExecutor = dbExecutor;
+    }
+
+    public async Task<List<InventoryReportDto>> GetAll(RequestParameter param)
+    {
+        string sp = "sp_Wms_InventoryReport_GetList";
+        return (await _dbExecutor.QueryListAsync<InventoryReportDto>(sp, param.ToQueryObject())).ToList();
+    }
+
+}
