e1538e2a5b2808b58389b9b76e46893e2c369cfb
mrizkyridwansyah <mrizkyridwansyah29@gmail.com>
2025-12-31 00:32:08 +0700

    update ui

--- Kawai.FE/pages/app/inventory-report/index.vue
+++ Kawai.FE/pages/app/inventory-report/index.vue
@@ -1,44 +1,48 @@
 <template>
   <v-frame title="Inventory Report" icon="table">
     <template #frame-content>
+      <table>
+        <tr>
+          <td><label class="form-label">Warehouse</label></td>
+          <td style="padding-left: 15px" colspan="3">
+            <filter-warehouse-privileges
+              class="form-control"
+              v-model="filter.warehouse"
+              factory-code="ALL"
+              style-code="width: 150px;"
+              style-desc="width: 250px;"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-top: 5px">
+            <label class="form-label">Area</label>
+          </td>
+          <td style="padding-top: 5px; padding-left: 15px" colspan="3">
+            <filter-area-privileges
+              class="form-control"
+              v-model="filter.area"
+              :warehouse="filter.warehouse"
+              style-code="width: 150px;"
+              style-desc="width: 250px;"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-top: 5px">
+            <label class="form-label">Period</label>
+          </td>
+          <td style="padding-top: 5px; padding-left: 15px; width: 200px;">
+            <input-month v-model="filter.period" style="width: 180px" />
+          </td>
+          <td style="padding-top: 5px;" colspan="2">
+            <div style="margin-left: -30px;">
+              <v-button-search-reset :search="search" :reset="reset" />
 
-      <!-- FILTER -->
-      <div class="row">
-        <label class="form-label col-form-label col-xl-1 col-lg-1 col-md-2">
-          Warehouse
-        </label>
-        <div class="col-xl-5 col-lg-5 col-md-10">
-          <filter-warehouse-privileges
-            class="form-control"
-            v-model="filter.warehouse"
-            factory-code="ALL"
-          />
-        </div>
-
-        <label class="form-label col-form-label col-xl-1 col-lg-1 col-md-2">
-          Area
-        </label>
-        <div class="col-xl-5 col-lg-5 col-md-10">
-          <filter-area-privileges
-            class="form-control"
-            v-model="filter.area"
-            :warehouse="filter.warehouse"
-          />
-        </div>
-      </div>
-
-      <div class="row mt-1">
-        <label class="form-label col-form-label col-xl-1 col-lg-1 col-md-2">
-          Period
-        </label>
-        <div class="col-xl-3 col-lg-3 col-md-6">
-          <input-month v-model="filter.period" />
-        </div>
-        <div class="col-xl-8 col-lg-8 col-md-6">
-          <v-button-search-reset :search="search" :reset="reset" />
-        </div>
-      </div>
-
+            </div>
+          </td>
+        </tr>
+      </table>
       <!-- TABLE -->
       <v-table
         :filter="filter"
@@ -47,7 +51,7 @@
         :export-excel="true"
         :export-excel-action="exportExcel"
       >
- syncing
+        syncing
         <template #table-content>
           <table
             class="table table-striped table-bordered mb-0 align-middle"
@@ -84,9 +88,13 @@
                 <td class="text-end">{{ $func.formatMoney(item.PreMonth) }}</td>
                 <td class="text-end">{{ $func.formatMoney(item.Receipt) }}</td>
                 <td class="text-end">{{ $func.formatMoney(item.Supply) }}</td>
-                <td class="text-end">{{ $func.formatMoney(item.LossReject) }}</td>
+                <td class="text-end">
+                  {{ $func.formatMoney(item.LossReject) }}
+                </td>
                 <td class="text-end">{{ $func.formatMoney(item.Current) }}</td>
-                <td class="text-end">{{ $func.formatMoney(item.Inventory) }}</td>
+                <td class="text-end">
+                  {{ $func.formatMoney(item.Inventory) }}
+                </td>
                 <td>{{ item.Remarks }}</td>
                 <td>{{ item.LastUser }}</td>
               </tr>
@@ -103,10 +111,10 @@ export default {
   data() {
     return {
       keywordKeys: [
-        { Id: 'ProductCode', Name: 'Product Code' },
-        { Id: 'ProductName', Name: 'Product Name' },
-        { Id: 'Warehouse', Name: 'Warehouse' },
-        { Id: 'LotNo', Name: 'Lot No' },
+        { Id: "ProductCode", Name: "Product Code" },
+        { Id: "ProductName", Name: "Product Name" },
+        { Id: "Warehouse", Name: "Warehouse" },
+        { Id: "LotNo", Name: "Lot No" },
       ],
 
       filter: {
@@ -124,22 +132,22 @@ export default {
 
         sortItems: [
           {
-            label: 'Product Code',
-            value: 'ProductCode',
+            label: "Product Code",
+            value: "ProductCode",
             selected: false,
-            direction: 'asc',
+            direction: "asc",
           },
           {
-            label: 'Product Name',
-            value: 'ProductName',
+            label: "Product Name",
+            value: "ProductName",
             selected: false,
-            direction: 'asc',
+            direction: "asc",
           },
           {
-            label: 'Warehouse',
-            value: 'Warehouse',
+            label: "Warehouse",
+            value: "Warehouse",
             selected: false,
-            direction: 'asc',
+            direction: "asc",
           },
         ],
       },
@@ -157,10 +165,10 @@ export default {
   },
 
   watch: {
-    'filter.keyword'() {
+    "filter.keyword"() {
       this.search();
     },
-    'filter.sorts'() {
+    "filter.sorts"() {
       this.search();
     },
   },
@@ -169,12 +177,10 @@ export default {
     search() {
       const filters = [
         {
-          WarehouseCode: this.filter.warehouse || '',
-          AreaCode: this.filter.area || '',
-          Period: this.$func.asUtcStringDateOnly(
-            new Date(this.filter.period)
-          ),
-          Keyword: this.filter.keyword || '',
+          WarehouseCode: this.filter.warehouse || "",
+          AreaCode: this.filter.area || "",
+          Period: this.$func.asUtcStringDateOnly(new Date(this.filter.period)),
+          Keyword: this.filter.keyword || "",
         },
       ];
 
--- Kawai.FE/pages/app/physical-inventory/index.vue
+++ Kawai.FE/pages/app/physical-inventory/index.vue
@@ -1,129 +1,136 @@
 <template>
-    <v-frame title="Physical Inventory" icon="receipt">
-        <template #frame-content>
-            <div class="row">
-                <label class="form-label col-form-label col-xl-1 col-lg-1 col-md-2 col-sm-2 col-xs-1">
-                    Warehouse
-                </label>
-                <div class="col-xl-5 col-lg-5 col-md-10 col-sm-10 col-xs-12">
-                    <filter-warehouse-privileges
-                        class="form-control"
-                        v-model="filter.warehouse"
-                        factory-code="ALL"
-                    />
-                </div>
+  <v-frame title="Physical Inventory" icon="receipt">
+    <template #frame-content>
+      <table>
+        <tr>
+          <td><label class="form-label">Warehouse</label></td>
+          <td style="padding-left: 15px" colspan="3">
+            <filter-warehouse-privileges
+              class="form-control"
+              v-model="filter.warehouse"
+              factory-code="ALL"
+              style-code="width: 170px;"
+              style-desc="width: 250px;"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-top: 5px">
+            <label class="form-label">Item</label>
+          </td>
+          <td style="padding-top: 5px; padding-left: 15px" colspan="3">
+          <filter-item
+            class="form-control"
+            placeholder="Search Item"
+            v-model="filter.item"
+              style-code="width: 170px;"
+              style-desc="width: 250px;"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-top: 5px">
+            <label class="form-label">Period</label>
+          </td>
+          <td style="padding-top: 5px; padding-left: 15px; width: 200px">
+            <input-month v-model="filter.period" style="width: 215px" />
+          </td>
+          <td style="padding-top: 5px" colspan="2">
+            <div style="margin-left: -40px">
+              <v-button-search-reset :search="search" :reset="reset" />
             </div>
-
-            <div class="row mt-1">
-                <label class="form-label col-form-label col-xl-1 col-lg-1 col-md-2 col-sm-2 col-xs-1">
-                    Item
-                </label>
-                <div class="col-xl-5 col-lg-5 col-md-10 col-sm-10 col-xs-12">
-                    <filter-item
-                        class="form-control"
-                        placeholder="Search Item"
-                        v-model="filter.item"
-                    />
-                </div>
-            </div>
-
-            <div class="row mt-1">
-                <label class="form-label col-form-label col-xl-1 col-lg-1 col-md-2 col-sm-2 col-xs-1">
-                    Period
-                </label>
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-6">
-                    <input-month
-                        v-model="filter.period"
-                        class="mr-1"
-                        placeholder="Period"
-                    />
-                </div>
-
-                <div class="col-xl-5 col-lg-5 col-md-4 col-sm-4 col-xs-5">
-                    <v-button-search-reset :search="search" :reset="reset" />
-                </div>
-            </div>
-            
-            <v-table
-              :filter="filter"
-              :export-excel="false"
-              :export-excel-action="exportExcel"
-              :frozen-column-left="4"
-              :data-items="ds.data.Items"
-              :ds="ds"
-              ref="vtable"
-            >
-              <template #table-content>
-                <table
-                  class="table table-striped table-bordered mb-0 align-middle v-fixed-table"
-                  v-if="!ds.isLoading && !ds.isNetworkError && !ds.isServerError"
-                  ref="table"
-                >
-                  <thead>
-                    <tr>
-                      <!-- <th class="text-center">Warehouse Code</th> -->
-                      <th class="text-center">Product Code</th>
-                      <th class="text-center">Description</th>
-                      <th class="text-center">Unit</th>
-                      <th class="text-center">Address</th>
-                      <th class="text-center">Pre Month Stock</th>
-                      <th class="text-center">Receipt Total</th>
-                      <th class="text-center">Supply Total</th>
-                      <th class="text-center">Loss / Reject</th>
-                      <th class="text-center">End Of Month Stock</th>
-                      <th class="text-center">Inventory</th>
-                      <th class="text-center">Diffrences</th>
-                      <th class="text-center">Reason</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="(item, idx) in ds.data.Items || []" :key="idx">
-                      <!-- <td>{{ item.WarehouseCode }}</td> -->
-                      <td>{{ item.ProductCode }}</td>
-                      <td>{{ item.ProductDesc }}</td>
-                      <td>{{ item.Unit }}</td>
-                      <td>{{ item.Address }}</td>
-                      <td class="text-right">{{ $func.formatDecimal(item.PreMonthStock) }}</td>
-                      <td class="text-right">{{ $func.formatDecimal(item.ReceiptTotal) }}</td>
-                      <td class="text-right">{{ $func.formatDecimal(item.SupplyTotal) }}</td>
-                      <td class="text-right">{{ $func.formatDecimal(item.Loss)}}</td>
-                      <td class="text-right">{{ $func.formatDecimal(item.EndOfMonthStock) }}</td>
-                      <td class="text-right">
-                        <input
-                          type="number"
-                          step="0.01"
-                          class="form-control form-control-sm text-right"
-                          v-model.number="item.Inventory"
-                          :ref="`inv-${idx}`"
-                          @focus="item.Inventory = Number(item.Inventory)"
-                          @blur="formatInventory(item)"
-                          @keyup.enter="onEnterInventory(item, idx)"
-                        />
-                      </td>
-                      <td class="text-right">
-                         {{ $func.formatDecimal(item.Inventory - item.EndOfMonthStock) }}
-                      </td>
-                      <td>
-                        <input
-                          type="string"
-                          class="form-control form-control-sm"
-                          placeholder="Input reason"
-                          :ref="`reason-${idx}`"
-                          v-model="item.Reason"
-                          @focus="item._oldReason = item.Reason"
-                          @keyup.enter="onEnterReason(item, idx)"
-                        />
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </template>
-            </v-table>
-            
+          </td>
+        </tr>
+      </table>
+      <v-table
+        :filter="filter"
+        :export-excel="false"
+        :frozen-column-left="4"
+        :data-items="ds.data.Items"
+        :ds="ds"
+        :use-header="false"
+        :use-paging="false"
+        ref="vtable"
+      >
+        <template #table-content>
+          <table
+            class="table table-striped table-bordered mb-0 align-middle v-fixed-table"
+            v-if="!ds.isLoading && !ds.isNetworkError && !ds.isServerError"
+            ref="table"
+          >
+            <thead>
+              <tr>
+                <!-- <th class="text-center">Warehouse Code</th> -->
+                <th class="text-center">Product Code</th>
+                <th class="text-center">Description</th>
+                <th class="text-center">Unit</th>
+                <th class="text-center">Address</th>
+                <th class="text-center">Pre Month Stock</th>
+                <th class="text-center">Receipt Total</th>
+                <th class="text-center">Supply Total</th>
+                <th class="text-center">Loss / Reject</th>
+                <th class="text-center">End Of Month Stock</th>
+                <th class="text-center">Inventory</th>
+                <th class="text-center">Diffrences</th>
+                <th class="text-center">Reason</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, idx) in ds.data.Items || []" :key="idx">
+                <!-- <td>{{ item.WarehouseCode }}</td> -->
+                <td>{{ item.ProductCode }}</td>
+                <td>{{ item.ProductDesc }}</td>
+                <td>{{ item.Unit }}</td>
+                <td>{{ item.Address }}</td>
+                <td class="text-right">
+                  {{ $func.formatDecimal(item.PreMonthStock) }}
+                </td>
+                <td class="text-right">
+                  {{ $func.formatDecimal(item.ReceiptTotal) }}
+                </td>
+                <td class="text-right">
+                  {{ $func.formatDecimal(item.SupplyTotal) }}
+                </td>
+                <td class="text-right">{{ $func.formatDecimal(item.Loss) }}</td>
+                <td class="text-right">
+                  {{ $func.formatDecimal(item.EndOfMonthStock) }}
+                </td>
+                <td class="text-right">
+                  <input
+                    type="number"
+                    step="0.01"
+                    class="form-control form-control-sm text-right"
+                    v-model.number="item.Inventory"
+                    :ref="`inv-${idx}`"
+                    @focus="item.Inventory = Number(item.Inventory)"
+                    @blur="formatInventory(item)"
+                    @keyup.enter="onEnterInventory(item, idx)"
+                  />
+                </td>
+                <td class="text-right">
+                  {{
+                    $func.formatDecimal(item.Inventory - item.EndOfMonthStock)
+                  }}
+                </td>
+                <td>
+                  <input
+                    type="string"
+                    class="form-control form-control-sm"
+                    placeholder="Input reason"
+                    :ref="`reason-${idx}`"
+                    v-model="item.Reason"
+                    @focus="item._oldReason = item.Reason"
+                    @keyup.enter="onEnterReason(item, idx)"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </template>
-    </v-frame>
+      </v-table>
+    </template>
+  </v-frame>
 </template>
-
 
 <script>
 export default {
@@ -140,7 +147,7 @@ export default {
       keyword: null,
       period: null,
       warehouseCode: null,
-      ItemCode: null,
+      item: null,
       sorts: {
         ItemCode: "asc",
       },
@@ -166,30 +173,30 @@ export default {
       this.rawData = [];
       this.treeData = [];
     },
-    "filter.warehouseCode": function () {
+    "filter.warehouse": function () {
       this.rawData = [];
       this.treeData = [];
     },
-    "filter.itemCode": function () {
+    "filter.item": function () {
       this.rawData = [];
       this.treeData = [];
-    }
+    },
   },
   mounted: function () {
     this.filter.period = new Date(
-        new Date().getFullYear(),
-        new Date().getMonth(),
-        1
-      ).toISOString();
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      1
+    ).toISOString();
     //this.getColumns();
   },
-  methods: {    
+  methods: {
     resetGrid: function () {
       this.ds.setFilter([]);
       this.ds.setPage(1);
       this.ds.setLength(10);
       this.ds.data.Items = [];
-    },    
+    },
     search: function () {
       this.ds.setSort(this.filter.sorts);
       let filters = [
@@ -205,11 +212,11 @@ export default {
       this.ds.load().then((dt) => (this.lists = dt.Data.Items));
     },
     reset: function () {
-      this.filter.WarehouseCode = null;
-      this.filter.ItemCode = null;
-      this.filter.Period = null;
+      this.filter.warehouse = null;
+      this.filter.item = null;
+      this.filter.period = null;
       this.search();
-    },  
+    },
     formatInventory(item) {
       let val = Number(item.Inventory || 0);
 
@@ -218,8 +225,8 @@ export default {
 
       item.Inventory = Number(val.toFixed(2));
     },
-    async onEnterInventory(item, idx) {   
-      console.log(idx);   
+    async onEnterInventory(item, idx) {
+      console.log(idx);
       // pindah ke inventory row berikutnya
       this.$nextTick(() => {
         const nextRef = this.$refs[`inv-${idx + 1}`];
@@ -231,14 +238,13 @@ export default {
       });
 
       this.formatInventory(item);
-      
+
       // kalau tidak berubah, skip update
       if (item._oldInventory !== item.Inventory) {
         await this.updateInventory(item);
       }
-
     },
-    async onEnterReason(item, idx) {  
+    async onEnterReason(item, idx) {
       console.log(idx);
       this.$nextTick(() => {
         const nextRef = this.$refs[`reason-${idx + 1}`];
@@ -246,33 +252,30 @@ export default {
           nextRef[0].focus();
         }
       });
-      
-      if ((item._oldReason || '') !== (item.Reason || '')) {
+
+      if ((item._oldReason || "") !== (item.Reason || "")) {
         await this.updateInventory(item);
       }
-
     },
     async updateInventory(item) {
       const payload = {
         WarehouseCode: item.WarehouseCode,
-        Period: this.$func.asUtcStringDateOnly(
-          new Date(this.filter.period)
-        ),
+        Period: this.$func.asUtcStringDateOnly(new Date(this.filter.period)),
         ProductCode: item.ProductCode,
         Inventory: item.Inventory,
-        Reason: item.Reason || ""
+        Reason: item.Reason || "",
       };
 
       try {
         await this.ds.updateInventory(payload);
         item._oldInventory = item.Inventory;
         item._oldReason = item.Reason;
-        toastSuccess('Data saved successfully!');
-      } catch (e) {        
-        toastDanger(e.Message)
+        toastSuccess("Data saved successfully!");
+      } catch (e) {
+        toastDanger(e.Message);
         console.error(e);
       }
-    }
+    },
   },
 };
 </script>
