3809829ea7fc593f0325b5d9eb87860806d94d4c
Rizal <rizal.fahlevi@tos.co.id>
2026-02-25 14:20:36 +0700

    <upd><ws setting>
    cek jika stoppoint sudah di set ke WSCode & LineCode

--- Kawai.Data/Repositories/WorkStationSettingRepository.cs
+++ Kawai.Data/Repositories/WorkStationSettingRepository.cs
@@ -1,10 +1,12 @@
-﻿using Kawai.Api.Models;
+﻿using Dapper;
+using Kawai.Api.Models;
 using Kawai.Data.SqlConnections;
 using Kawai.Domain.DTOs;
 using Kawai.Domain.Interfaces;
 using Kawai.Domain.Models;
 using Kawai.Domain.Shared;
 using System.Data;
+using System.Diagnostics;
 
 namespace Kawai.Data.Repositories;
 
@@ -32,6 +34,33 @@ public class WorkStationSettingRepository : IWorkStationSettingRepository
     public async Task SaveWorkStationSetting(WorkStationSetting wssettinglist, string userId)
     {
         var commands = new List<(string, object?, CommandType)>();
+        //ini list StopPoint dari payload
+        var stopPoints = wssettinglist.SettingList
+            .Where(x => x.AllowSetting == true && !string.IsNullOrEmpty(x.StopPointCode))
+            .Select(x => x.StopPointCode)
+            .Distinct()
+            .ToList();
+        
+        // =========================
+        // Build Table Valued Parameter
+        // =========================
+        var table = new DataTable();
+        table.Columns.Add("StopPointCode", typeof(string));
+        foreach (var sp in stopPoints)
+        {
+            table.Rows.Add(sp);
+        }
+
+        var parameters = new DynamicParameters();
+        parameters.Add("@StopPoints", table.AsTableValuedParameter("dbo.tvp_StopPointList"));
+        parameters.Add("@LineCode", wssettinglist.LineCode);
+
+        commands.Add((
+           "sp_WMS_WorkStationSetting_CheckStopPoints",
+           parameters,
+           CommandType.StoredProcedure
+        ));
+
 
         commands.Add(("sp_WMS_WorkStationSetting_Delete", new
         {
---
+++ Kawai.Data/Sql/Db/User-Defined-Table-Types/tvp_StopPointList.sql
@@ -0,0 +1,4 @@
+CREATE TYPE dbo.tvp_StopPointList AS TABLE
+(
+    StopPointCode NVARCHAR(MAX)
+);
