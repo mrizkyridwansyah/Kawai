5bfeb1b15d85545e42e574fabf51dfbff075c904
rahadianreza <rr.kahfi@gmail.com>
2026-04-08 14:09:43 +0700

    <upd> UserGuide

--- Kawai.FE/nuxt.config.ts
+++ Kawai.FE/nuxt.config.ts
@@ -1,17 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ["pdfjs-dist", "vuetify"]
+   
+  },
+
+  vite: {
+    optimizeDeps: {
+      include: ["pdfjs-dist/legacy/build/pdf"]
+    }
+  },
+
   devtools: { enabled: true },
   ssr: false,
+
   experimental: {
     payloadExtraction: false
   },
+
   app: {
-    // pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       title: 'TOS App',
     },
   },
-  transpile: ["vuetify"],
+
   modules: [
     '@bootstrap-vue-next/nuxt',
     [
@@ -21,13 +32,15 @@ export default defineNuxtConfig({
       },
     ],
   ],
+
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
     '@/assets/app.scss',
     '@/assets/default/app.min.css',
     '@/assets/css/vendor.min.css'
   ],
+
   imports: {
     dirs: ['stores', 'stores/data-sources/**', 'stores/forms'],
   },
-})
+});
