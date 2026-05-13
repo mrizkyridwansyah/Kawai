0a48e861e9f5722cd1ca2e867b9c06c8b7ad2061
mrizkyridwansyah <mrizkyridwansyah29@gmail.com>
2026-05-13 11:04:26 +0700

    update example hide top navbar

--- Kawai.Andon.FE/pages/app/request-control/index.vue
+++ Kawai.Andon.FE/pages/app/request-control/index.vue
@@ -10,28 +10,31 @@
     <div class="filter-area p-3" id="containerfilter">
       <!--dropdown area-->
       <div class="row">
-       
-          <div class="row mt-1">
-            <label
-              class="form-label col-form-label col-xl-1 col-lg-1 col-md-2 col-sm-2 col-xs-1">Area</label>
-              
-            <div class="col-sm-6">
-              <filter-area
-                class="form-control"
-                v-model="filter.area"
-                v-model:area-name="filter.areaName"
-                warehouse=""
-                ></filter-area>
-            </div>
-              <div style="margin-top: -5px;" class="form-label col-form-label col-xl-1 col-lg-1 col-md-2 col-sm-2"> <v-button-search class="ms-1" :search="search"  /></div>
+        <div class="row mt-1">
+          <label
+            class="form-label col-form-label col-xl-1 col-lg-1 col-md-2 col-sm-2 col-xs-1"
+            >Area</label
+          >
+
+          <div class="col-sm-6">
+            <filter-area
+              class="form-control"
+              v-model="filter.area"
+              v-model:area-name="filter.areaName"
+              warehouse=""
+            ></filter-area>
           </div>
-         
+          <div
+            style="margin-top: -5px"
+            class="form-label col-form-label col-xl-1 col-lg-1 col-md-2 col-sm-2"
+          >
+            <v-button-search class="ms-1" :search="search" />
+          </div>
         </div>
-        
- 
+      </div>
     </div>
 
-    <div class="panel-body" id="panelbody" >
+    <div class="panel-body" id="panelbody">
       <div class="row">
         <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
           <div
@@ -60,19 +63,17 @@
         <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
           <div
             class="p-2 header-summary-content"
-            style="background-color: #18B2E0"
+            style="background-color: #18b2e0"
           >
             <span class="title-summary">Picking Progress (Item)</span>
             <br />
-            <span class="qty-summary">{{
-               this.summary.totalItem 
-            }}</span>
+            <span class="qty-summary">{{ this.summary.totalItem }}</span>
           </div>
         </div>
         <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
           <div
             class="p-2 header-summary-content"
-            style="background-color: #18B2E0"
+            style="background-color: #18b2e0"
           >
             <span class="title-summary">Remaining Item</span>
             <br />
@@ -91,33 +92,34 @@
                 icon="hourglass-start"
                 class="text-warning icon-title"
               />
-              
-              <span id="header-panel" style="font-size: 1.05em" class="ml-3">Remaining Item - Material Type (Group)</span>
+
+              <span id="header-panel" style="font-size: 1.05em" class="ml-3"
+                >Remaining Item - Material Type (Group)</span
+              >
             </div>
             <div class="panel-body">
-                 <table
-                  class="table mb-0 align-middle w-100 v-fixed-table"
-                  ref="table"
-                >
+              <table
+                class="table mb-0 align-middle w-100 v-fixed-table"
+                ref="table"
+              >
                 <thead>
                   <tr>
                     <th class="text-center">Request No</th>
                     <th class="text-center">Production Date</th>
                     <th class="text-center">Line</th>
-                    <th class="text-center">Item</th> 
-                     <th class="text-center">Model</th> 
+                    <th class="text-center">Item</th>
+                    <th class="text-center">Model</th>
                     <th class="text-center">Work Station</th>
                     <th class="text-center">Preparation Status</th>
                     <th class="text-center">Trolly Number</th>
                     <th class="text-center">Current Position</th>
                     <th class="text-center">Next Location</th>
-              
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="(item, i) in ds.data" >
+                  <tr v-for="(item, i) in ds.data">
                     <td>{{ item.RequestNo }}</td>
-                    <td>{{ $func.formatDate(item.ProductionDate)}}</td>
+                    <td>{{ $func.formatDate(item.ProductionDate) }}</td>
                     <td>{{ item.Line }}</td>
                     <td>{{ item.PickingArea }}</td>
                     <td>{{ item.Model }}</td>
@@ -126,12 +128,10 @@
                     <td>{{ item.TrollyNumber }}</td>
                     <td>{{ item.CurrentPosition }}</td>
                     <td>{{ item.NextLocation }}</td>
-                     
-                    
                   </tr>
                 </tbody>
               </table>
-              <v-data-empty class="mt-3" v-if="ds.data.Length==0"/>
+              <v-data-empty class="mt-3" v-if="ds.data.Length == 0" />
             </div>
           </div>
         </div>
@@ -150,12 +150,11 @@ export default {
       remaining: 0,
     },
     listRemaining: [],
-    filter: ({
+    filter: {
       // warehouse: null,
       area: null,
-      areaName: ""    
-    })
-     
+      areaName: "",
+    },
   }),
   computed: {
     ds: function () {
@@ -168,9 +167,7 @@ export default {
     // },
     "filter.area": function () {
       this.filter.area;
-
     },
-   
   },
   mounted: async function () {
     this.search();
@@ -189,50 +186,55 @@ export default {
     // });
   },
   methods: {
-     search: function () {
-     
+    search: function () {
       let filters = [
         {
           AreaCode: this.filter.area || "",
-          
         },
       ];
       this.ds.load(this.filter.area);
-       this.ds
+      this.ds
         .load(this.filter.area)
         .then((dt) => {
-          debugger
+          debugger;
           this.list = dt.Data;
           //total diambil dari countdata datalist
           this.summary.total = this.list.length;
-          this.summary.totalItem = this.list.length > 0  ? this.list[0].PickingProgress   : 0;
-          this.summary.remaining = this.list.length > 0  ? this.list[0].Remaining   : 0;
+          this.summary.totalItem =
+            this.list.length > 0 ? this.list[0].PickingProgress : 0;
+          this.summary.remaining =
+            this.list.length > 0 ? this.list[0].Remaining : 0;
 
           //untuk womin summary semua workstasion yang sama aja
-          this.summary.womin = this.list.length > 0  ? this.list[0].Womin   : 0;
+          this.summary.womin = this.list.length > 0 ? this.list[0].Womin : 0;
           //   index === self.findIndex((t) => (
           //     t.WorkStation === item.WorkStation
           //   ))
           // ).length;
-
-
-
-
-        }).catch((err) => {
+        })
+        .catch((err) => {
           console.error("Error loading data:", err);
         });
-      
-      if (this.filter.area!=null) {
+
+      if (this.filter.area != null) {
         //header-panel change text to "Remaining Item" dan nama area bukan codenya
         const headerPanel = document.getElementById("header-panel");
         if (headerPanel) {
           headerPanel.innerText = `Remaining Item - Material Type (${this.filter.areaName})`;
         }
-              
+
         //hide filter id="containerfilter" after search
         const containerFilter = document.getElementById("containerfilter");
         const panelBody = document.getElementById("panelbody");
         if (containerFilter && panelBody) {
+
+          /* HIDE TOP NAVBAR
+          const header = document.getElementById("header");
+          const appContent = document.getElementById("app");
+          header.style.display = "none";
+          appContent.style.paddingTop = "0";
+          */
+
           containerFilter.style.display = "none";
           panelBody.style.marginTop = "0px";
         }
@@ -245,23 +247,21 @@ export default {
             panelBody.style.marginTop = "20px";
           }
         });
-        
       }
-
     },
     reset: function () {
       this.filter.area = null;
       this.search();
     },
-    
+
     //load: function () {
     //   this.ds.load().then((dt) => (this.listRemaining = dt.Data));
-      // this.ds.loadSummary().then((dt) => {
-      //   this.summary.total = dt.Data.Total;
-      //   this.summary.womin = dt.Data.Womin;
-      //   this.summary.item = dt.Data.Item;
-      //   this.summary.remaining = dt.Data.Remaining;
-      // });
+    // this.ds.loadSummary().then((dt) => {
+    //   this.summary.total = dt.Data.Total;
+    //   this.summary.womin = dt.Data.Womin;
+    //   this.summary.item = dt.Data.Item;
+    //   this.summary.remaining = dt.Data.Remaining;
+    // });
     //},
   },
 };
@@ -296,7 +296,6 @@ export default {
   border: 1px solid gainsboro !important;
   /* border-collapse: separate; */
   /* border-spacing: 0; */
-   
 }
 
 .v-fixed-table th,
@@ -305,7 +304,6 @@ export default {
   padding: 8px 16px;
   border: 1px solid #dee2e6;
   background: #fff;
-  
 }
 
 /* Sticky Header (atas) */
